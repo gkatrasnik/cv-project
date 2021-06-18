d7505524dee0cfed54a1a9d1692400c22ab5677a
Gasper Katrasnik <gkatrasnik@gmail.com>
2021-06-18 14:14:44 +0200

    personal component made

--- src/components/Personal.js
+++ src/components/Personal.js
@@ -5,9 +5,52 @@ import PersonalView from "./PersonalView";
 export default class Personal extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      fullName: "",
+      address: "",
+      phone: "",
+      email: "",
+      showForm: true,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.toggleShowForm();
+  };
+
+  toggleShowForm = () => {
+    const showForm = this.state;
+    this.setState({ showForm: !showForm });
+  };
   render() {
-    return <h1>personal</h1>;
+    const { fullName, address, phone, email, showForm } = this.state;
+
+    return showForm ? (
+      <PersonalForm
+        fullName={fullName}
+        address={address}
+        phone={phone}
+        email={email}
+        handleChange={this.handleChange}
+        handleSubmit={this.handleSubmit}
+      />
+    ) : (
+      <PersonalView
+        fullName={fullName}
+        address={address}
+        phone={phone}
+        email={email}
+        toggleShowForm={this.toggleShowForm}
+      />
+    );
   }
 }
